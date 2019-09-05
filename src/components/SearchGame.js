9fe6c2f411747c9aad718388684407c143c41f3d
nicholasstano <nicholasstano@gmail.com>
2019-09-05 17:38:19 -0400

    Filter GAmes

--- src/components/SearchGame.js
+++ src/components/SearchGame.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-export default function SearchGame() {
+export default function SearchGame(props) {
     return (
         <div>
-            <input type="text" class="form-control" placeholder="Search Game by Name" />
+            <input type="text" className="form-control" placeholder="Search Game by Name" onChange={(event) => props.setSearchGameState(event.target.value)} value={props.value} />
         </div>
     )
 }
--- src/container/Games.js
+++ src/container/Games.js
@@ -5,14 +5,21 @@ import SearchGame from '../components/SearchGame'
 export class Games extends Component {
     state = { searchGame: "" }
 
+    setSearchGameState = search => {
+        this.setState({ searchGame: search })
+    }
 
+    filterGames = () => {
+        let filteredGames = this.props.allGames.filter(game => game.name.toLowerCase().includes(this.state.searchGame.toLowerCase()))
+        return filteredGames
+    }
 
     render() {
-        let gameComponents = this.props.allGames.map(game => <GameCard game={game} />)
+        let gameComponents = this.filterGames().map(game => <GameCard game={game} />)
         return (
             <div>
                 <samp>
-                    <SearchGame />
+                    <SearchGame value={this.state.searchGame} setSearchGameState={this.setSearchGameState} />
                     {gameComponents}
                 </samp>
             </div>
