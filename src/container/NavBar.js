d1d98861ca4f86afb8349b61915f2cdab67af335
Colin <cstatro@gmail.com>
2019-09-11 14:38:20 -0400

    title commit

--- src/container/NavBar.js
+++ src/container/NavBar.js
@@ -22,44 +22,82 @@ export class NavBar extends Component {
         {!this.props.user ? (
           <div>
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
-              <Link className="navbar-brand" to="#">Navbar</Link>
-              <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation" >
+              <Link className="navbar-brand" to="#">
+                Dice To Meet You
+              </Link>
+              <button
+                className="navbar-toggler"
+                type="button"
+                data-toggle="collapse"
+                data-target="#navbarNav"
+                aria-controls="navbarNav"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+              >
                 <span className="navbar-toggler-icon"></span>
               </button>
               <ul className="navbar-nav">
-                <li className="nav-item"><Link className="nav-link" to="/login">Login / Signup</Link></li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/login">
+                    Login / Signup
+                  </Link>
+                </li>
               </ul>
             </nav>
           </div>
         ) : (
-            <div>
-              <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <Link className="navbar-brand" to="#">Navbar</Link>
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation" >
-                  <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarNav">
-                  <ul className="navbar-nav">
-                    <li className="nav-item">
-                      <Link className="nav-link" to="/home">Home</Link>
-                    </li>
-                    <li className="nav-item">
-                      <Link className="nav-link" to="/meetup/new">New Meetup</Link>
-                    </li>
-                    <li className="nav-item">
-                      <Link className="nav-link" to="/meetups">Open Meetups</Link>
-                    </li>
-                    <li className="nav-item">
-                      <Link className="nav-link" to="/games">Games</Link>
-                    </li>
-                    <li className="nav-item">
-                      <Link className="nav-link" to="#" onClick={this.handleLogout} >Logout</Link>
-                    </li>
-                  </ul>
-                </div>
-              </nav>
-            </div>
-          )}
+          <div>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+              <Link className="navbar-brand" to="#">
+                Dice To Meet You
+              </Link>
+              <button
+                className="navbar-toggler"
+                type="button"
+                data-toggle="collapse"
+                data-target="#navbarNav"
+                aria-controls="navbarNav"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+              >
+                <span className="navbar-toggler-icon"></span>
+              </button>
+              <div className="collapse navbar-collapse" id="navbarNav">
+                <ul className="navbar-nav">
+                  <li className="nav-item">
+                    <Link className="nav-link" to="/home">
+                      Home
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link className="nav-link" to="/meetup/new">
+                      New Meetup
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link className="nav-link" to="/meetups">
+                      Open Meetups
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link className="nav-link" to="/games">
+                      Games
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link
+                      className="nav-link"
+                      to="#"
+                      onClick={this.handleLogout}
+                    >
+                      Logout
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            </nav>
+          </div>
+        )}
       </div>
     );
   }
